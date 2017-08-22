881b3ed0dbaa05aea6b2d1405611f33202a59304
Kristopher Wojciechowski <kristopher.wojciechowski@gmail.com>
2017-08-22 15:31:37 +0200

    slabo to widze :/

--- Script1.js
+++ Script1.js
@@ -1,1 +1,10 @@
 // JavaScript source code
+<script>
+    var superVar = 5;
+    superVar = superVar + 2;
+    console.log(superVar); //wypisze sie 7
+
+    var superVar = 5;
+    var superVar2 = 3;
+    console.log(superVar + superVar2); //wypisze sie 8
+</script>
